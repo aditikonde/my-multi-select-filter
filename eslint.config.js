7db2cdb798327e5caad0859ad30b8754c3b6a90f
aditikonde <aditi.konde.25@gmail.com>
2025-08-30 20:37:27 +0200

    prettier plugin added

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +14,17 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      eslintConfigPrettier, // disable ESLint formatting rules and enable Prettier
     ],
+    plugins: {
+      prettier, // add prettier
+    },
+    rules: {
+      'prettier/prettier': 'error', // show Prettier issues in ESLint
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
