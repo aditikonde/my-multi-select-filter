4ef66be8dbacbcdde8559ef54273ca2301e66e6a
aditikonde <aditi.konde.25@gmail.com>
2025-09-01 23:46:28 +0200

    decoding html entities to normal text

--- server/service.js
+++ server/service.js
@@ -3,7 +3,11 @@ import itemsList from '../src/assets/items.json' with { type: 'json' };
 export const loadItems = () => {
   let items = [];
   try {
-    items = Array.isArray(itemsList.data) ? itemsList.data : [];
+    items = Array.isArray(itemsList.data)
+      ? itemsList.data.map((str) =>
+          str.replace(/&amp;/g, '&').replace(/&#x27;/g, "'")
+        )
+      : [];
   } catch (err) {
     console.error('Error loading items.json:', err);
   }
