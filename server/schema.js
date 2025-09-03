08247f7c2c2fbba17a13f2da9943736c992b2888
aditikonde <aditi.konde.25@gmail.com>
2025-09-03 23:42:20 +0200

    Fixed import error for typedef (lowecase D)

--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,5 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { typeDefs } from './typeDefs.js';
+import { typeDefs } from './typedefs.js';
 import { resolvers } from './resolvers.js';
 
 export const schema = makeExecutableSchema({
